7f64dc813fcdec19e7e2ef677848c63cbd77b876
AaronOBrien1453 <aatobrien@gmail.com>
2020-12-10 14:49:15 -0500

    deploy to heroku

--- project/settings.py
+++ project/settings.py
@@ -146,10 +146,7 @@ INTERNAL_IPS = [
     # ...
 ]
 
-# Configure Django App for Heroku
-import django_heroku
-django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+
 
 # Registration and login
 
@@ -171,3 +168,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
